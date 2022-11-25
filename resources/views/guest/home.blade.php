f16f6f60ac9948c026ecffc2a0081ad15dc80e55
Smurrowz <nigeldn1995@gmail.com>
2022-11-25 17:07:59 +0100

    vue router

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,20 +1,21 @@
-@extends('layouts.app')
-@section('content')
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
+
 <body>
-  <div id="app">
-
-  </div>
-  <script src="{{asset('js/front.js')}}" defer></script>
+    @extends('layouts.app')
+    @section('content')
+    @endsection
+    <div id="app">
+    
+    </div>
+    <script src="{{ asset('js/front.js') }}" defer></script>
 </body>
-</html>
 
-@endsection
+</html>
