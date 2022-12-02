38b8cf4a9eba5eee99ea0a49d16fb68342d5fe5a
Bilobra <bra.carrozzo@gmail.com>
2022-12-02 09:32:47 +0100

    fix searchbar + deatils doctor

--- resources/js/components/SearchBarSel.vue
+++ resources/js/components/SearchBarSel.vue
@@ -1,54 +1,59 @@
 <template>
     <div>
-        <div class="position">
-            <div class="row">
-                <div class="col-12 pt-4 pl-5">
-                    <h4>Cerca un medico per specializzazione</h4>
-                    <select name="" id="" v-model="filterText" class="form-control">
-                        <option value="">Scegli la specializzazione</option>
-                        <option :value="specialization.specialization" v-for="specialization in SpecsArray"
-                            :key="specialization.id">
-                            {{ specialization.specialization }}</option>
-                    </select>
-                    <button @click="filter()">
+
+        <div class="row">
+            <div class="col-12 col-md-4">
+                <h4>Cerca un medico per specializzazione</h4>
+                <select name="" id="" v-model="filterText" class="form-control">
+                    <option value="">Scegli la specializzazione</option>
+                    <option :value="specialization.specialization" v-for="specialization in SpecsArray"
+                        :key="specialization.id">
+                        {{ specialization.specialization }}</option>
+                </select>
+                <router-link :to="{ name: 'search'}">
+                    <button @click=filter()>
                         Cerca
                     </button>
-                </div>
-            </div>
-            <div class="row py-5">
-                <div v-for="doc in filteredbySpec" class="card mx-3 " style="width: 18rem;" :key="doc.id">
-                    <div class="img-box">
-                        <img v-if="doc.image" :src="`/storage/${doc.image}`" class="card-img-top" alt="...">
-                        <img src="http://mascitelliandpartners.com/map/wp-content/uploads/2015/03/placeholder_user.png"
-                            class="card-img-top" v-else>
-    
-                    </div>
-    
-                    <div class="card-body">
-                        {{ doc.name }} {{ doc.surname }}
-    
-                        <ul>
-                            <li v-for="spec in doc.specializations" :key="spec.id">
-                                {{ spec.specialization }}
-    
-                            </li>
-                        </ul>
-    
-                        <p v-if=" doc.reviews.length <= 0">nessuna recensione</p>
-                        <p v-else-if="doc.reviews.length == 1">{{doc.reviews.length}} recensione</p>
-                        <p v-else>{{doc.reviews.length}} recensioni</p>
-    
-                        <p v-if="doc.stars.length <= 0">nessun voto disponibile</p>
-                        <p v-else>{{doc.avg}}</p>
-                        <router-link :to="{ name: 'doctor.details', params: {id: doc.id }}">show</router-link>
-                    </div>
-                </div>
+                </router-link>
             </div>
         </div>
+
+
+
+        <!-- <div class="row py-5">
+            <div v-for="doc in filteredDoctors" class="card mx-3 " style="width: 18rem;">
+                <div class="img-box">
+                    <img v-if="doc.image" :src="`/storage/${doc.image}`" class="card-img-top" alt="...">
+                    <img src="http://mascitelliandpartners.com/map/wp-content/uploads/2015/03/placeholder_user.png"
+                        class="card-img-top" v-else>
+
+                </div>
+
+                <div class="card-body">
+                    {{ doc.name }} {{ doc.surname }}
+
+                    <ul>
+                        <li v-for="spec in doc.specializations" :key="spec.id">
+                            {{ spec.specialization }}
+
+                        </li>
+                    </ul>
+
+                    <p v-if=" doc.reviews.length <= 0">nessuna recensione</p>
+                    <p v-else-if="doc.reviews.length == 1">{{doc.reviews.length}} recensione</p>
+                    <p v-else>{{doc.reviews.length}} recensioni</p>
+
+                    <p v-if="doc.stars.length <= 0">nessun voto disponibile</p>
+                    <p v-else>{{doc.avg}}</p>
+                    <router-link :to="{ name: 'doctor.details', params: {id: doc.id }}">show</router-link>
+                </div>
+            </div>
+        </div> -->
     </div>
 </template>
 
 <script>
+import state from '../store.js';
 export default {
     name: "SearchBarSel",
     data() {
@@ -58,84 +63,63 @@ export default {
             filteredbySpec: [],
             specs: [],
             SpecsArray: [],
-
-
         }
+    },
+    computed:{
+      doctorsComputed(){
+          console.log(state.doctors , 'dottori computed')
+        return state.doctors
+      },
+      filteredDoctors(){
+        console.log(state.filteredDocs, 'dottori filrati computed')
+        return state.filteredDocs
+      }
     },
     beforeMount() {
         this.fetchSpecs()
-
-    },
-    created(){
         this.fetchDoctors()
-
     },
+   
     methods: {
         fetchDoctors() {
             axios.get('/api/users').then((res) => {
-                this.doctorsArray = res.data.result
-                console.log(this.doctorsArray)
+               state.doctors = res.data.result
+                // console.log(this.doctorsArray)
             })
-
-
         },
         fetchSpecs() {
             axios.get('/api/specializations').then((res) => {
                 this.SpecsArray = res.data
-                console.log(this.SpecsArray)
+                // console.log(this.SpecsArray)
             })
         },
         filter() {
-            this.filteredbySpec = [],
-                this.doctorsArray.forEach(el => {
+            state.filteredDocs = [],
+                state.doctors.forEach(el => {
                     el.visible = false
                     this.specs = el.specializations
                     el.total = 0
                     el.stars.forEach(star => {
                         el.total += star.vote
-
                         // console.log(star)
                     });
                     el.avg =  Math.floor(el.total/el.stars.length)
                     // el.avg = Math.round(2)
                     console.log(this.filterText)
                     this.specs.forEach(spec => {
-
                         if (spec.specialization == this.filterText) {
                             el.visible = true
-                            this.filteredbySpec.push(el)
+                            state.filteredDocs.push(el)
                         }
                     });
                 });
-            console.log(this.filteredbySpec)
+            // console.log(this.filteredbySpec)
         },
-        // reset() {
-        //     this.filteredbySpec = []
-        //     this.filterText = ''
-        // },
-
-        // vote(doctor) {
-        //     let total = []
-        //     // console.log(doc)
-        //     doctor.stars.forEach(star => {
-        //         total += star.vote
-        //     });
-
-        //     console.log(total)
-
-        // }
+        
     },
 }
 </script>
 
 <style lang="scss" scoped>
-.position{
-    position: absolute;
-    select{
-        max-width: 300px;
-    }
-}
-h4{
-    color: white;
-}
+
 </style>
--- resources/js/pages/AdvancedSearch.vue
+++ resources/js/pages/AdvancedSearch.vue
@@ -1,23 +1,124 @@
 <template>
-  <div>
-   
+  <div class="container py-5">
+    <div class=" p-5 row_doctors row row-cols-2 row-cols-lg-4 row-cols-sm-1 row-cols-md-3 gy-1">
+      <div v-if="filteredDoctors.length == 0">
+        nessun dottore
+      </div>
+      <div v-for="doc in filteredDoctors" class=" col card card_doctors " style="width: 18rem;">
+        <div class="img-box py-2">
+          <img v-if="doc.image" :src="`/storage/${doc.image}`" class="card-img-top" alt="...">
+          <img src="http://mascitelliandpartners.com/map/wp-content/uploads/2015/03/placeholder_user.png"
+            class="card-img-top" v-else>
+
+        </div>
+
+        <div class="card-body">
+          <div class="card_title">
+            {{ doc.name }} {{ doc.surname }}
+          </div>
+
+
+          <div>
+            |<span v-for="spec in doc.specializations" :key="spec.id">
+              {{ spec.specialization }} |
+
+            </span>
+          </div>
+
+          <p v-if="doc.reviews.length <= 0">nessuna recensione</p>
+          <p v-else-if="doc.reviews.length == 1">{{ doc.reviews.length }} recensione</p>
+          <p v-else>{{ doc.reviews.length }} recensioni</p>
+
+          <p v-if="doc.stars.length <= 0">nessun voto disponibile</p>
+          <p v-else>media voti : {{ doc.avg }}</p>
+          <div class="button_div">
+            <router-link :to="{ name: 'doctor.details', params: { id: doc.id } }">
+              <button class="button-details">
+                Dettagli
+              </button>
+            </router-link>
+
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-
+import state from '../store.js';
 export default {
   name: 'AdvancedSearch',
-  data(){
+  data() {
     return {
-      
+      filterText: '',
+      doctorsArray: [],
+      filteredbySpec: [],
+      specs: [],
+      SpecsArray: [],
     }
   },
-  methods: {
-    
+  computed: {
+    doctorsComputed() {
+      return state.doctors
+    },
+    filteredDoctors() {
+      return state.filteredDocs
+    }
   },
-
+  beforeMount() {
+    // this.fetchSpecs()
+    this.fetchDoctors()
+  },
+  methods: {
+    fetchDoctors() {
+      axios.get('/api/users').then((res) => {
+        this.doctorsArray = res.data.result
+        console.log(this.doctorsArray)
+      })
+    },
+  },
 }
 </script>
 <style scoped lang="scss">
+.row_doctors {
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 40px;
 
+  .button-details {
+    padding: 5px 14px;
+    border-radius: 20px;
+    background-color: #186f79b5;
+    border: none;
+    color: white;
+    font-size: 18px;
+    transition: all 300ms;
+
+    &:hover {
+      // transform: scale(1.02);
+      background-color: #186f79d8;
+      // border: 1px solid white;
+    }
+  }
+
+  .card_doctors {
+    box-shadow: 0px 4px 6px #0000008a;
+    // height: 500px;
+    padding: 20px;
+    // width: 400px;
+    // height: 600px;
+  }
+
+  .card-body {
+    display: flex;
+    gap: 10px;
+    flex-direction: column;
+    // justify-content: center;
+  }
+}
+
+.card_title {
+  font-weight: 500;
+  text-transform: uppercase;
+}
 </style>
